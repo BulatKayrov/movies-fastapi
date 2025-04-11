c0a104384a1ea0c98be99d05d39954a078a8c920
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-12 00:52:53 +0400

    first view

--- movie-app/main.py
+++ movie-app/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title='Movies',
@@ -7,6 +7,12 @@ app = FastAPI(
     description='Movie app',
     docs_url='/docs',
 )
+
+
+@app.get('/')
+async def root(request: Request):
+    path = request.url.replace(path='/docs')
+    return {'docs': path}
 
 if __name__ == '__main__':
     uvicorn.run(app)
