d9eca68489156033cd99e5c37239ff4ed31dcfd1
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-13 17:57:14 +0400

    create method POST for create movies

--- movie-app/api/v1/movie/views.py
+++ movie-app/api/v1/movie/views.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+import random
+from typing import Annotated
+
+from fastapi import APIRouter, Form
 from fastapi.params import Depends
 
 from api.v1.movie.dependecies import DATABASE, find_movie_by_id
@@ -14,4 +17,16 @@ async def get_all_movies():
 
 @router.get(path="/{movie_id}", response_model=SMovie)
 async def get_one_movie(movie=Depends(find_movie_by_id)):
+    return movie
+
+
+@router.post(path="/", response_model=SMovie)
+async def create_one_movie(
+    title: Annotated[str, Form(min_length=1, max_length=50)],
+    description: Annotated[str, Form(min_length=1, max_length=50)],
+    year: Annotated[int, Form(ge=1, le=1_000_000)],
+):
+    pk = random.randint(1, 1_000_000)
+    movie = SMovie(movie_id=pk, title=title, description=description, year=year)
+    DATABASE.append(movie)
     return movie
