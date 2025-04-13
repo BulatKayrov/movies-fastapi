5574fad41e9824030e1745ff0b0a63f495470b6c
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-13 17:33:58 +0400

    create APIRouter, crud file and other

--- movie-app/api/v1/movie/dependecies.py
+++ movie-app/api/v1/movie/dependecies.py
@@ -1,11 +1,6 @@
+from api.v1.movie.crud import DATABASE
 from api.v1.movie.schemas import SMovie
 from fastapi import HTTPException, status
-
-DATABASE = [
-    SMovie(movie_id=1, title="Terminator 1", description="Nice film 1", year=1999),
-    SMovie(movie_id=2, title="Terminator 2", description="Nice film 2", year=2000),
-    SMovie(movie_id=3, title="Terminator 3", description="Nice film 3", year=2001),
-]
 
 
 async def find_movie_by_id(movie_id: int):
