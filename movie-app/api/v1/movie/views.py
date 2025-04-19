a12bfe66e2c79a5d3529d3ca142ff612642c1d4b
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-19 19:02:12 +0400

    add background task

--- movie-app/api/v1/movie/views.py
+++ movie-app/api/v1/movie/views.py
@@ -25,30 +25,38 @@ def get_one_movie(movie=Depends(find_movie_by_slug)):
 @router.post(path="/", response_model=SMovie)
 def create_one_movie(background_task: BackgroundTasks, data: SMovieCreate):
     background_task.add_task(storage.save())
-    logger.info('Movie created')
+    logger.info("Movie created")
     return storage.create(data=data)
 
 
 @router.delete(
     path="/{slug}", responses={**RESPONSES}, status_code=status.HTTP_204_NO_CONTENT
 )
-def delete_one_movie(background_task: BackgroundTasks, movie=Depends(find_movie_by_slug)):
+def delete_one_movie(
+    background_task: BackgroundTasks, movie=Depends(find_movie_by_slug)
+):
     background_task.add_task(storage.save())
     storage.delete_record(movie=movie)
-    logger.info('Movie deleted')
+    logger.info("Movie deleted")
 
 
 @router.put(path="/{slug}", response_model=SMovie)
-def update_one_movie(movie_in: SMovieUpdate, background_task: BackgroundTasks, movie=Depends(find_movie_by_slug)):
+def update_one_movie(
+    movie_in: SMovieUpdate,
+    background_task: BackgroundTasks,
+    movie=Depends(find_movie_by_slug),
+):
     background_task.add_task(storage.save())
-    logger.info('Movie updated')
+    logger.info("Movie updated")
     return storage.update_record(movie=movie, movie_in=movie_in)
 
 
 @router.patch(path="/{slug}", response_model=SMovie)
 def partial_update(
-    movie_in: SMoviePartialUpdate, background_task: BackgroundTasks, movie=Depends(find_movie_by_slug)
+    movie_in: SMoviePartialUpdate,
+    background_task: BackgroundTasks,
+    movie=Depends(find_movie_by_slug),
 ):
     background_task.add_task(storage.save())
-    logger.info('Movie updated')
+    logger.info("Movie updated")
     return storage.update(movie=movie, movie_in=movie_in, partial=True)
