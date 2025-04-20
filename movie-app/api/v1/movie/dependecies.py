34ee4b095f3ac08405f543efb5255c30c21f02a8
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-20 13:56:50 +0400

    httpdearer

--- movie-app/api/v1/movie/dependecies.py
+++ movie-app/api/v1/movie/dependecies.py
@@ -1,12 +1,17 @@
 import logging
 
-from fastapi import HTTPException, status, BackgroundTasks, Request, Header
+from fastapi import HTTPException, status, BackgroundTasks, Request, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from api.v1.movie.crud import storage
 from core.config import settings
 
 logger = logging.getLogger(__name__)
 UNSAFE_METHODS = frozenset({"POST", "PUT", "PATCH", "DELETE"})
+
+static_api_token = HTTPBearer(
+    auto_error=False, scheme_name="Static API token", description="Static API token"
+)
 
 
 def find_movie_by_slug(slug: str):
@@ -19,12 +24,16 @@ def find_movie_by_slug(slug: str):
 
 
 def get_token(
-    request: Request, api_token: str = Header(default="", alias="x-auth-token")
+    api_token: HTTPAuthorizationCredentials | None = Depends(static_api_token),
 ):
-    if request.method not in UNSAFE_METHODS:
-        return
+    logger.info("api token required %s", api_token)
+    if not api_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required.",
+        )
 
-    if api_token not in settings.API_TOKENS:
+    if api_token.credentials not in settings.API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API token"
         )
