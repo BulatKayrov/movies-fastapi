27a0a4e3e3efa4e7e43f1f6607a9fcf643730d90
BulatKayrov <bulat.kajrov@yandex.ru>
2025-07-08 22:49:58 +0400

    add pytest-coverage

--- movie-app/testing/conftest.py
+++ movie-app/testing/conftest.py
@@ -2,5 +2,9 @@ from os import getenv
 
 import pytest
 
-if getenv("testing") != "1":
-    pytest.exit("Environment is not ready for testing")
+
+@pytest.fixture(scope="session", autouse=True)
+def check_testing_env() -> None:
+    if getenv("testing") != "1":
+        pytest.exit("Environment is not ready for testing")
+        # pytest.fail("Environment is not ready for testing")
