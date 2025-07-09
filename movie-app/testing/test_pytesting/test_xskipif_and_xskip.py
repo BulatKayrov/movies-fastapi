025695039b41b0fcf9921ab5d0236898ff013f68
BulatKayrov <bulat.kajrov@yandex.ru>
2025-07-09 15:40:18 +0400

    add first GH Actions

---
+++ movie-app/testing/test_pytesting/test_xskipif_and_xskip.py
@@ -0,0 +1,20 @@
+import sys
+
+import pytest
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_other_schema():
+    other_schema = {"username": "No name"}
+    assert other_schema["username"] == "Bob"
+
+
+@pytest.mark.skipif(sys.version_info < (3, 12), reason="requires Python 3.12 or higher")
+def test_other_schema_skipif():
+    pass
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="requires platform win32 or higher")
+def test_other_schema_skipif_v2():
+    """Пропустить этот тест, если платформа Windows (потому что требуется не-Windows или более новая версия)"""
+    assert sys.platform != "win32"
